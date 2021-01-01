1ce59883b41bf9951d629f4cd774cdf1a98652e4
Diksha Kapoor <dkapoor@tekion.com>
2021-01-01 19:05:10 +0530

    adding winner logic

--- src/App.js
+++ src/App.js
@@ -9,26 +9,26 @@ class App extends Component {
     }
   }
 
-  // checkWinner(){
-  //   const winLines = [
-  //     ["0","1","2"],
-  //     ["3","4","5"],
-  //     ["6","7","8"],
-  //     ["0","3","6"],
-  //     ["1","4","7"],
-  //     ["2","5","8"],
-  //     ["0","4","8"],
-  //     ["2","4","6"],
-  //   ];
+  checkWinner(){
+    const winLines = [
+      ["0","1","2"],
+      ["3","4","5"],
+      ["6","7","8"],
+      ["0","3","6"],
+      ["1","4","7"],
+      ["2","5","8"],
+      ["0","4","8"],
+      ["2","4","6"],
+    ];
 
-  //   for (let index = 0; index < winLines.length; index){
-  //     const [firstWinningIndex, secondWinningIndex, thirdWinningIndex] = winLines[index];
-  //     const {board} = this.state;
-  //     if (board[firstWinningIndex] && board[firstWinningIndex] === board[secondWinningIndex] && board[firstWinningIndex] === board[thirdWinningIndex]){ //board[firstWinningIndex] &&  board[secondWinningIndex] &&  board[thirdWinningIndex]
-  //       alert("you won")
-  //     }
-  //   }
-  // }
+    for (let index = 0; index < winLines.length; index++){
+      const [firstWinningIndex, secondWinningIndex, thirdWinningIndex] = winLines[index];
+      const {board} = this.state;
+      if (board[firstWinningIndex]&& board[secondWinningIndex] && board[thirdWinningIndex] && board[firstWinningIndex] === board[secondWinningIndex] && board[firstWinningIndex] === board[thirdWinningIndex]){ //board[firstWinningIndex] &&  board[secondWinningIndex] &&  board[thirdWinningIndex]
+        alert("you won")
+      }
+    }
+  }
 
   handleBoxClick(index){
     const {board, player} = this.state;
@@ -40,7 +40,7 @@ class App extends Component {
       player : newPlayer,
     })
   }
-  //this.checkWinner()
+  this.checkWinner()
   }
 
 render(){
